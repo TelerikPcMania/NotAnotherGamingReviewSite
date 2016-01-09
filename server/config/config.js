47440f1b1e48cca86aa09640157e53ec1284de0a
vyara <vyara_hristova@yahoo.de>
2016-01-09 16:44:31 +0200

    removed unnecessary comments from config.js

--- server/config/config.js
+++ server/config/config.js
@@ -4,12 +4,12 @@ var rootPath = path.normalize(__dirname + '/../../')
 module.exports = {
     development: {
         rootPath: rootPath,
-        db: 'mongodb://localhost:27017/pcmania', //db connection string to be added
+        db: 'mongodb://localhost:27017/pcmania',
         port: process.env.PORT || 2020
     },
     production: {
         rootPath: rootPath,
-        db: 'mongodb://pcmania:pcmania@ds039185.mongolab.com:39185/heroku_2j8dwp6n', //heroku mongolab connection string
+        db: 'mongodb://pcmania:pcmania@ds039185.mongolab.com:39185/heroku_2j8dwp6n',
         port: process.env.PORT || 2020
     }
 }
