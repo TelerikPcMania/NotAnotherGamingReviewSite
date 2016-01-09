5c4a7ed6a0da755cb4c2f0ad5a30418c21d299bc
vyara <vyara_hristova@yahoo.de>
2016-01-09 11:57:21 +0200

    added some basic port configuration and express dependency in server.js

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
-/**
- * Created by Vyara on 1/9/2016.
- */
+var express = require('express');
+
+var env = process.env.NODE_ENV || 'development';
+
+var app = express();
+var config = require('./server/config/config')[env];
+
+//custom model routes to be added
+
+app.listen(config.port);
+console.log("Server running on port: " + config.port);
