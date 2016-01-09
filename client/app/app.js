a988c4f2c3eb6c15c436744ba173d4c9916a9d37
vyara <vyara_hristova@yahoo.de>
2016-01-09 23:13:10 +0200

    added some basic app.js routes and configs logic, more to be added later

--- client/app/app.js
+++ client/app/app.js
@@ -1,1 +1,53 @@
 var app = angular.module('app', ['ngResource', 'ngRoute']).value('toastr', toastr);
+
+app.config(function($routeProvider) {
+
+    var routeUserChecks = {
+        adminRole: {
+            authenticate: function(auth) {
+                return auth.isAuthorizedForRole('admin');
+            }
+        },
+        authenticated: {
+            authenticate: function(auth) {
+                return auth.isAuthenticated();
+            }
+        }
+    };
+
+    $routeProvider
+        .when('/', {
+            templateUrl: '/partials/main/home',
+            controller: 'MainCtrl'
+        })
+        .when('/games', {
+            templateUrl: '/partials/games/games-list',
+            controller: 'GamesListCtrl'
+        })
+        .when('/courses/:id', {
+            templateUrl: '/partials/games/game-details',
+            controller: 'GameDetailsCtrl'
+        })
+        .when('/signup', {
+            templateUrl: '/partials/account/signup',
+            controller: 'SignUpCtrl'
+        })
+        .when('/profile', {
+            templateUrl: '/partials/account/profile',
+            controller: 'ProfileCtrl',
+            resolve: routeUserChecks.authenticated
+        })
+        .when('/admin/users', {
+            templateUrl: '/partials/admin/users-list',
+            controller: 'UserListCtrl',
+            resolve: routeUserChecks.adminRole
+        })
+});
+
+app.run(function($rootScope, $location) {
+    $rootScope.$on('$routeChangeError', function(ev, current, previous, rejection) {
+        if (rejection === 'not authorized') {
+            $location.path('/');
+        }
+    })
+});
