fc71ae6cc48c662d30c4ed78988b62d05a6d3c72
vyara <vyara_hristova@yahoo.de>
2016-01-09 19:14:11 +0200

    small correction to the localhost db string

--- server/config/config.js
+++ server/config/config.js
@@ -4,7 +4,7 @@ var rootPath = path.normalize(__dirname + '/../../')
 module.exports = {
     development: {
         rootPath: rootPath,
-        db: 'mongodb://localhost:27017/pcmania',
+        db: 'mongodb://localhost/pcmania',
         port: process.env.PORT || 2020
     },
     production: {
