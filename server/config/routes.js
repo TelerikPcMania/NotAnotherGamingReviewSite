52678498e5047b6132ac4f0e4c64bdb80983d33e
Ivan Nikolov <ivan.stoyanov.nikolov@gmail.com>
2016-01-10 23:24:46 +0200

    Added user login and register - working locally

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,9 +1,16 @@
-var controllers = require('../controllers');
+var auth = require('./auth'),
+    controllers = require('../controllers');
 //TODO: modify routes if needed
 module.exports = function(app) {
+    app.get('/api/users', auth.isInRole('admin'), controllers.users.getAllUsers);
+    app.post('/api/users', controllers.users.createUser);
+    app.put('/api/users', auth.isAuthenticated, controllers.users.updateUser);
 
     app.get('/api/games', controllers.games.getAllGames);
     app.get('/api/games/:id', controllers.games.getGamesById);
+
+    app.post('/login', auth.login);
+    app.post('/logout', auth.logout);
 
     app.get('/partials/:partialArea/:partialName', function(req, res) {
         res.render('../../client/app/' + req.params.partialArea + '/' + req.params.partialName)
