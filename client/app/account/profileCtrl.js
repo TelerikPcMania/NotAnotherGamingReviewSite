bf620711677682051fb722c4e76411b6ab0c0dc6
Ivan Nikolov <ivan.stoyanov.nikolov@gmail.com>
2016-01-11 00:29:48 +0200

    Added some user tuning

--- client/app/account/profileCtrl.js
+++ client/app/account/profileCtrl.js
@@ -1,13 +1,15 @@
 app.controller('ProfileCtrl', function($scope, $location, auth, identity) {
     $scope.user = {
         firstName: identity.currentUser.firstName,
-        lastName: identity.currentUser.lastName
+        lastName: identity.currentUser.lastName,
+        userName: identity.currentUser.username
     }
 
     $scope.update = function(user) {
         auth.update(user).then(function() {
             $scope.firstName = user.firstName;
             $scope.lastName = user.lastName;
+            $scope.userName = user.username;
             $location.path('/');
         });
     }
