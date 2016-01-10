927eb31093965ba967158f82235ed00468b2e37b
Ivan Nikolov <ivan.stoyanov.nikolov@gmail.com>
2016-01-10 21:26:20 +0200

    Revert "added more user logic"
    This reverts commit f2ca308a2165c5bc3b7a729c19c1685e4603547b.

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,12 +1,10 @@
 var controllers = require('../controllers');
 //TODO: modify routes if needed
 module.exports = function(app) {
-    //app.get('/api/users', auth.isInRole('admin'), controllers.users.getAllUsers);
-    //app.post('/api/users', controllers.users.createUser);
-    //app.put('/api/users', auth.isAuthenticated, controllers.users.updateUser);
 
     app.get('/api/games', controllers.games.getAllGames);
     app.get('/api/games/:id', controllers.games.getGamesById);
+    app.get('/login', controllers.users.getUserById);
     app.get('/partials/:partialArea/:partialName', function(req, res) {
         res.render('../../client/app/' + req.params.partialArea + '/' + req.params.partialName)
     });
@@ -14,7 +12,4 @@ module.exports = function(app) {
     app.get('*', function(req, res) {
         res.render('index', {currentUser: req.user});
     });
-
-    app.post('/login', auth.login);
-    //app.post('/logout', auth.logout);
 }
