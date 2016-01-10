c3c19cf0084d7e500e664f1c1d16b5731594765d
vyara <vyara_hristova@yahoo.de>
2016-01-10 15:52:30 +0200

    fixed an issue with date of the initially seeded games

--- server/models/game.js
+++ server/models/game.js
@@ -23,17 +23,17 @@ module.exports.seedInitialGames = function() {
         }
 
         if (collection.length === 0) {
-            Game.create({title: 'Fallout 4', featured: true, released: new Date('10/11/2015'), tags: ['Post-apocalyptic', 'Open World', 'Exploration', 'RPG'],
+            Game.create({title: 'Fallout 4', featured: true, released: new Date('11/10/2015'), tags: ['Post-apocalyptic', 'Open World', 'Exploration', 'RPG'],
                 platforms: ['PC', 'PS4', 'Xbox One'], rating: '0', reviews: {}});
-            Game.create({title: 'Grand Theft Auto V', featured: true, released: new Date('14/4/2015'), tags: ['Open World', 'Action', 'Multiplayer', 'Third Person'],
+            Game.create({title: 'Grand Theft Auto V', featured: true, released: new Date('4/14/2015'), tags: ['Open World', 'Action', 'Multiplayer', 'Third Person'],
                 platforms: ['PC', 'PS4', 'Xbox One'], rating: '0', reviews: {}});
-            Game.create({title: 'Mass Effect 2', featured: true, released: new Date('29/1/2010'), tags: ['RPG', 'Sci-fi', 'Story Rich', 'Third Person Shooter'],
+            Game.create({title: 'Mass Effect 2', featured: true, released: new Date('1/29/2010'), tags: ['RPG', 'Sci-fi', 'Story Rich', 'Third Person Shooter'],
                 platforms: ['PC', 'PS4', 'Xbox One'], rating: '0', reviews: {}});
-            Game.create({title: 'Just Cause 3', featured: true, released: new Date('1/12/2015'), tags: ['Open World', 'Action', 'SDestruction', 'Adventure'],
+            Game.create({title: 'Just Cause 3', featured: true, released: new Date('12/1/2015'), tags: ['Open World', 'Action', 'SDestruction', 'Adventure'],
                 platforms: ['PC', 'PS4', 'Xbox One'], rating: '0', reviews: {}});
-            Game.create({title: 'Portal 2', featured: true, released: new Date('19/4/2011'), tags: ['Puzzle', 'Co-op', 'First-Person', 'Comedy', 'Sci-fi'],
+            Game.create({title: 'Portal 2', featured: true, released: new Date('4/19/2011'), tags: ['Puzzle', 'Co-op', 'First-Person', 'Comedy', 'Sci-fi'],
                 platforms: ['PC', 'PS4', 'Xbox One'], rating: '0', reviews: {}});
-            Game.create({title: 'The Witcher 3: Wild Hunt', featured: true, released: new Date('18/5/2015'), tags: ['RPG', 'Open World', 'Story Rich', 'Atmospheric'],
+            Game.create({title: 'The Witcher 3: Wild Hunt', featured: true, released: new Date('5/18/2015'), tags: ['RPG', 'Open World', 'Story Rich', 'Atmospheric'],
                 platforms: ['PC', 'PS4', 'Xbox One'], rating: '0', reviews: {}});
             console.log('Games added to database...');
         }
